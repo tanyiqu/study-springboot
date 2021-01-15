dff9e2fb0509b6e1935d54f1301bcb7fde566040
Tanyiqu <1953649096@qq.com>
2021-01-15 13:08:05 +0800

    第一个springboot项目

---
+++ springboot-study/boot-01-start/src/test/java/love/tanyiqu/boot/Boot01StartApplicationTests.java
@@ -0,0 +1,13 @@
+package love.tanyiqu.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot01StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
